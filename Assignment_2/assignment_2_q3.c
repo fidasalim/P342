67c8e54374bd82bec7b0e2378eb32085969483c9
fidasalim <42141530+fidasalim@users.noreply.github.com>
2020-08-18 16:30:32 +0530

    Add files via upload

--- Assignment_2/assignment_2_q3.c
+++ Assignment_2/assignment_2_q3.c
@@ -5,45 +5,45 @@ int main(){
 	//reading file A.txt
 	FILE *A_matrix;
 	A_matrix=fopen("A.txt", "r");
-	float A[3][1]={0.0};
-    	printf("The matrix A is [");
-    	for(i=0 ;i<3; i++){
-    		printf("[");
-        	for(j=0; j<1; j++){
-            		fscanf(A_matrix, "%f%*c",&A[i][j]);
-            		printf("%f",A[i][j]);
-        	}
-        	printf("],");
-    	}
-    	printf("]\n");
+    float A[3][1]={0.0};
+    printf("The matrix A is [");
+    for(i=0 ;i<3; i++){
+    	printf("[");
+        for(j=0; j<1; j++){
+            fscanf(A_matrix, "%f%*c",&A[i][j]);
+            printf("%f",A[i][j]);
+        }
+        printf("],");
+    }
+    printf("]\n");
     
-    	//reading file M.txt 
-    	FILE *M_matrix;
+    //reading file M.txt 
+    FILE *M_matrix;
 	M_matrix=fopen("M.txt", "r");
-    	float M[3][3]={0.0};
-    	printf("The matrix M is [");
-   	for(i=0 ;i<3; i++){
-    		printf("[");
-        	for(j=0; j<3; j++){
-           		fscanf(M_matrix, "%f%*c%*",&M[i][j]);
-            		printf("%f,",M[i][j]);
-        	}
-        	printf("],");
+    float M[3][3]={0.0};
+    printf("The matrix M is [");
+    for(i=0 ;i<3; i++){
+    	printf("[");
+        for(j=0; j<3; j++){
+            fscanf(M_matrix, "%f%*c%*",&M[i][j]);
+            printf("%f,",M[i][j]);
+        }
+        printf("],");
 	}
 	printf("]\n");
 	
 	//reading file N.txt 
-    	FILE *N_matrix;
+    FILE *N_matrix;
 	N_matrix=fopen("N.txt", "r");
-	float N[3][3]={0.0};
-    	printf("The matrix N is [");
-    	for(i=0 ;i<3; i++){
-    		printf("[");
-        	for(j=0; j<3; j++){
-            		fscanf(N_matrix, "%f%*c%*",&N[i][j]);
-            		printf("%f,",N[i][j]);
-        	}
-       		 printf("],");
+    float N[3][3]={0.0};
+    printf("The matrix N is [");
+    for(i=0 ;i<3; i++){
+    	printf("[");
+        for(j=0; j<3; j++){
+            fscanf(N_matrix, "%f%*c%*",&N[i][j]);
+            printf("%f,",N[i][j]);
+        }
+        printf("],");
 	}
 	printf("]\n");
 	
@@ -52,12 +52,14 @@ int main(){
 
 	printf("The matrix MxN is [");
 	for(i=0;i<3;i++){
+		printf("[");
 		for(j=0;j<3;j++){
 			for(k=0;k<3;k++){
 				O[i][j]+= M[i][k]*N[k][j];	
 			}
 			printf("%f,",O[i][j]);
 		}	
+		printf("],");
 	}
 	printf("]\n");
 	
@@ -83,6 +85,6 @@ int main(){
 The matrix A is [[2.000000],[-3.500000],[4.000000],]
 The matrix M is [[1.000000,2.000000,-3.000000,],[4.000000,-5.000000,6.000000,],[0.000000,8.000000,9.000000,],]
 The matrix N is [[4.000000,9.000000,-7.000000,],[0.000000,6.000000,2.500000,],[9.000000,-1.000000,8.000000,],]
-The matrix MxN is [-23.000000,24.000000,-26.000000,70.000000,0.000000,7.500000,81.000000,39.000000,92.000000,]
+The matrix MxN is [[-23.000000,24.000000,-26.000000,],[70.000000,0.000000,7.500000,],[81.000000,39.000000,92.000000,]]
 The matrix MxA is [[-17.000000,],[49.500000,],[8.000000,],]
 */   
