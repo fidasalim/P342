6e2ff5ccb33eb6b45234c1ac9b6752af33bf0a7a
fidasalim <42141530+fidasalim@users.noreply.github.com>
2020-09-12 15:45:31 +0530

    Add files via upload

--- Assignment_4/assignment_4_q1.py
+++ Assignment_4/assignment_4_q1.py
@@ -5,6 +5,7 @@ Created on Mon Sep  7 19:28:16 2020
 @author: fida
 """
 #Q1_solution
+print('Solution_Q1')
 
 #importing the libraries
 import library_assignment4 as lib
@@ -16,26 +17,27 @@ Q1_A , Q1_B = lib.seperate_matrix(Q1_M)
 
 #LU_decomposition
 Q1_P,Q1_Q = lib.LU_decomposition(Q1_A,Q1_B)
-print(Q1_P)
+print('Compined LU matrix is A = {}'.format(Q1_P))
+
 #forward and backward substitution(combined)
 Q1_X = lib.substitution(Q1_P,Q1_Q)
-
-print('Solution_Q1')
 print('Solution for AX = B, {}[x1,x2,x3,x4] = {} is\n[x1,x2,x3,x4] = {}\n'.format(Q1_A,Q1_B,Q1_X))
 
-
+"""
+"""
 #Q2_solution
+print('Solution_Q2')
+
 #reading the matrix 
 Q2_A = lib.read_matrix('q2.txt')
 Q2_B = [i for i in range(len(Q2_A))]
+print('The matrix A = {}'.format(Q2_A))
 
 #LU_decomposition
 Q2_P,Q2_Q = lib.LU_decomposition(Q2_A,Q2_B)
 
 #finding determinant of the triangular_matrix
 det_Q2_A = lib.triangularmatrix_determinant(Q2_P)
-print('Solution_Q2')
-print('The matrix A = {}'.format(Q2_A))
 print('Deteminant of A =', format(det_Q2_A))
 print('The determinant of A is not equal to zero,thus det(A) exists\n')
 
@@ -43,17 +45,18 @@ print('The determinant of A is not equal to zero,thus det(A) exists\n')
 Q2_Y = [[0 for y in range(len(Q2_A))]for x in range(len(Q2_A))]
 I = [0.0 for j in range(len(Q2_A))]
 for i in range(len(Q2_A)):
-    I[i-3] = 1.0
+    n = Q2_Q[i]
+    I[i] = 1.0
     for k in range(len(I)):
-        Q2_Y[k][i] = lib.substitution(Q2_P,I)[k]
+        Q2_Y[k][n] = round(lib.substitution(Q2_P,I)[k],3)
     I = [0 for l in range(len(Q2_A))]
     
 print('The inverse of A is {}'.format(Q2_Y))
-print('The A*A^-1 = {} = I_4'.format(lib.matrix_multiplication(Q2_A,Q2_Y)))
+print('Then A*A^-1 = {} = I_4'.format(lib.matrix_multiplication(Q2_A,Q2_Y)))
 
 """
-Solution
 Solution_Q1
+Compined LU matrix is A = [[1.0, 0.0, 1.0, 2.0], [0.0, 1.0, -2.0, 0.0], [1.0, 2.0, 2.0, -2.0], [2.0, 1.0, 1.5, -3.0]]
 Solution for AX = B, [[1.0, 0.0, 1.0, 2.0], [0.0, 1.0, -2.0, 0.0], [1.0, 2.0, -1.0, 0.0], [2.0, 1.0, 3.0, -2.0]][x1,x2,x3,x4] = [6.0, -3.0, -2.0, 0.0] is
 [x1,x2,x3,x4] = [1.0, -1.0, 1.0, 2.0]
 
@@ -62,6 +65,6 @@ The matrix A = [[0.0, 2.0, 8.0, 6.0], [0.0, 0.0, 1.0, 2.0], [0.0, 1.0, 0.0, 1.0]
 Deteminant of A = 36.0
 The determinant of A is not equal to zero,thus det(A) exists
 
-The inverse of A is [[-0.25000000000000006, 1.6666666666666672, -1.8333333333333333, 0.3333333333333333], [0.08333333333333337, -0.666666666666667, 0.8333333333333333, 0.0], [0.16666666666666666, -0.33333333333333326, -0.3333333333333333, 0.0], [-0.08333333333333333, 0.6666666666666666, 0.16666666666666666, 0.0]]
-The A*A^-1 = [[1.0, 0, 0, 0], [0, 1.0, 0, 0], [0, 0, 0.9999999999999999, 0], [0, 0, 0, 1.0]] = I_4
+The inverse of A is [[-0.25, 1.667, -1.833, 0.333], [0.083, -0.667, 0.833, 0.0], [0.167, -0.333, -0.333, 0.0], [-0.083, 0.667, 0.167, 0.0]]
+Then A*A^-1 = [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [-0.0, -0.0, -0.0, 1.0]] = I_4
 """
