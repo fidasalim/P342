f85bc0ac13831cba66fb43c85c5cf9d6ce5b85e0
fidasalim <42141530+fidasalim@users.noreply.github.com>
2020-11-28 21:00:03 +0530

    Add files via upload

--- Assignment_7/Assignment_7.py
+++ Assignment_7/Assignment_7.py
@@ -15,13 +15,20 @@ f3 = lib.f3
 Euler_method = lib.Explicit_Euler_method
 Shooting_method = lib.shooting_method
 RK_4 = lib.Runge_Kutta4_method_2_O
-Euler_method(f1_1,100,2,e,0.1)
-Euler_method(f1_2,100,3,1,0.1)
+Euler_method(f1_1,1,100,2,e,0.1)
+Euler_method(f1_2,2,100,3,1,0.1)
 RK_4(f2,100,0,5,2,1)
 Shooting_method(f3, 100, 0, 1, 1, 2*(e-1))
 
 """
 Solution:
-Guess_1 the slope at y(0):-3
-Guess_2 the slope at y(0):6
+Question_3
+Guess_1 the slope at y(0):10
+The guess a smaller slope
+
+Guess_1 the slope at y(0):0
+
+Guess_2 the slope at y(0):3
+
+Using langrange extrapolation, the slope at y(0) is 0.9999978720203273
 """
--- Assignment_7/Assignment_7_library.py
+++ Assignment_7/Assignment_7_library.py
@@ -14,20 +14,18 @@ f2 = lambda z,y,x : -z + 1 - x
 f3 = lambda z,y,x : z + 1
 setattr(f1a,'name','dy/dx=ylog(y)/x')
 setattr(f1b,'name','dy/dx=6-(2y/x)')
-setattr(f2,'name','d2y/dx2 + dy/dx = 1 - x')
-setattr(f3,'name','d2y/dx2 = dy/dx + 1')
-
+setattr(f2,'name','d2y/dx2 dy/dx=1-x')
+setattr(f3,'name','d2y/dx2=dy/dx+1')
+    
 #Explicit_Euler_method     
 def Explicit_Euler_method(u,N,x_0,y_0,h):
     X = [x_0]
     Y = [y_0]
     for n in range(N):
-        #small increment of x
-        x = X[-1]+h
-        #new y due to dx increment
+        x = X[-1]+h 
         y = Y[-1]+h*u(Y[-1],X[-1])
         X.append(x)
-        Y.append(y)  
+        Y.append(y)
     return(X,Y)  
 
 #Runge_Kutta4_method_for second order DE
@@ -63,6 +61,7 @@ def shooting_method(u,N,a,b,y_a,y_b):
         M,N1 = Runge_Kutta4_method_2_O(u,N,a,b,y_a,g_1)
         #coorecting slope 1 to ve less than 0
         while (round(N1[-1],5) >y_b):
+            print('The guess a smaller slope')
             g_1 = float(input('Guess_1 the slope at y({}):'.format(a)))
             M,N1 = Runge_Kutta4_method_2_O(u,N,a,b,y_a,g_1)   
         if round(N1[-1] - y_b,5)!= 0:
@@ -71,9 +70,11 @@ def shooting_method(u,N,a,b,y_a,y_b):
             M,N2 = Runge_Kutta4_method_2_O(u,N,a,b,y_a,g_2)
             #correcting slope to be greater than 0
             while (round(N2[-1],5)<y_b):
+                print('The guess a larger slope')
                 g_2 = float(input('Guess_2 the slope at y({}):'.format(a)))
                 M,N2 = Runge_Kutta4_method_2_O(u,N,a,b,y_a,g_2)
             #lagrange_interpolation
             l = g_1 + (g_2 - g_1)*(y_b-N1[-1])/(N2[-1]-N1[-1])
+            print('Using langrange extrapolation, the slope at y({}) is {}'.format(a,l))
             M,N1 = Runge_Kutta4_method_2_O(u,N,a,b,y_a,l)
         return(M,N1)
