0fb2d11e4cee34474d4c84a6f91824b101f3016a
fidasalim <42141530+fidasalim@users.noreply.github.com>
2021-02-03 19:40:25 +0530

    Add files via upload

--- Project/Q_2_Monte_Carlo/Q_2_montecarlo.py
+++ Project/Q_2_Monte_Carlo/Q_2_montecarlo.py
@@ -18,9 +18,9 @@ for n in N_list:
     #finding fractional error
     frac_error.append((Error/Vol))
 #comparing the analytical value with original value through plot
-plot_graph1(N_list,Volume,'Step_size(N)','Volume','Volume_vs_Step_size_(N)','Q_2_comparison',4*m.pi*1*1.5*2/3)
+#plot_graph1(N_list,Volume,'Step_size(N)','Volume','Volume_vs_Step_size_(N)','Q_2_comparison',4*m.pi*1*1.5*2/3)
 #plotting for fractional error
-plot_graph1(N_list,frac_error,'Step_size(N)','fractionl_error','Fractional_error_vs_Step_size_(N)','Q_2_frac_error','NA')
+#plot_graph1(N_list,frac_error,'Step_size(N)','fractionl_error','Fractional_error_vs_Step_size_(N)','Q_2_frac_error','NA')
 #plotting the points for N = 20000
 X_in, Y_in, Z_in = Monte_carlo_Volume(f1,1,1.5,2,20000)
-threeDplot(X_in ,Y_in , Z_in , 'Monte_Carlo_Elipsoid','Elipsoid_using_Monte_Carlo_for_N=10000',1,1.5,2)
+#threeDplot(X_in ,Y_in , Z_in , 'Monte_Carlo_Elipsoid','Elipsoid_using_Monte_Carlo_for_N=10000',1,1.5,2)
